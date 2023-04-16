4c28b21202d450870b8bc39e02828cab54d41a81
xtakac09 <xtakac09@stud.fit.vutbr.cz>
2023-04-16 15:09:12 +0200

    upravy pro NIX2

--- ipk-sniffer.c
+++ ipk-sniffer.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
-#include <types.h>
+#include <sys/types.h>
 #include <pcap.h>
 #include <arpa/inet.h>
 #include <net/ethernet.h>   // pro praci s hlavickami -> ether_header
